dc0a3eff2a470d639f081446a5673c95efd31667
Thiago Mateus <thiagomateus123@hotmail.com>
2022-03-04 17:07:21 -0300

    Classe de Conta criada

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPOOContaBancaria.Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string? message) : base(message)
+        {
+        }
+    }
+}
