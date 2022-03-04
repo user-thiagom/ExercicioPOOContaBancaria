da1a3573081e85f74857f2ef5f233ccd69a5ab11
Thiago Mateus <thiagomateus123@hotmail.com>
2022-03-04 17:19:44 -0300

    Finalização do Programa

--- Entities/Account.cs
+++ Entities/Account.cs
@@ -11,15 +11,15 @@ namespace ExercicioPOOContaBancaria.Entities
     {
         public int Number { get; set; }
         public string Holder { get; set; }
-        public double Balance { get; set; }
+        public double Balance { get; private set; }
         public double WithdrawLimit { get; set; }
 
         public Account(int number, string holder, double balance, double withdrawLimit)
         {
             Number = number;
             Holder = holder;
-            Balance = balance;
             WithdrawLimit = withdrawLimit;
+            Deposit(balance);
         }
 
         public override string ToString()
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExercicioPOOContaBancaria.Entities;
+using ExercicioPOOContaBancaria.Entities.Exceptions;
+
+Console.WriteLine("Enter account data");
+Console.Write("Number: ");
+int accountNumber = int.Parse(Console.ReadLine());
+Console.Write("Holder: ");
+string accountHolder = Console.ReadLine();
+Console.Write("Initial balance: ");
+double accountBalance = double.Parse(Console.ReadLine());
+Console.Write("Withdraw limit: ");
+double accountWithdrawLimit = double.Parse(Console.ReadLine());
+
+Account account = new Account(accountNumber,accountHolder,accountBalance,accountWithdrawLimit);
+
+try
+{
+    Console.Write("Enter amount for withdraw: ");
+    account.Withdraw(double.Parse(Console.ReadLine()));
+    Console.Write($"New balance: {account.Balance}");
+}
+catch (DomainException e)
+{
+    Console.WriteLine(e.Message);
+}
